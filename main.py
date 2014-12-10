645b52f02c7fbfa10d3dd0a28cc7cad95e89f27f
BryanTabarez <userbryan@gmail.com>
2014-12-10 11:06:11 -0500

    Actualizando...

--- main.py
+++ main.py
@@ -15,22 +15,26 @@ def main():
        #'2014-01-01', 'corredor de bolsa', 900)
     #daoPac.guardarPaciente(paciente)
 
-    ##PRUEBA BORRAR PACIENTE
-    #daoPac.borrarPaciente(110)
-
     ##PRUEBA CONSULTAR PACIENTE
     #paciente = daoPac.consultarPaciente(110)
     #print((paciente.get_identificacion()))
     #print((paciente.get_nombre()))
     #print((paciente.get_num_seg_social()))
 
-    ##PRUEBA MODIFICAR PACIENTE
+    ##PRUEBA MODIFICAR PACIENTE --> FALTA
+
+    ##PRUEBA BORRAR PACIENTE
+    #daoPac.borrarPaciente(110)
 
     ##PRUEBA INSERTAR ENFERMERA
     daoEnfe = DaoEnfermera(conexion)
     angely = Enfermera(114230, "Angelly", "calle 45", "3108304383", 1,
-        "angelly@correo.com", 2000000, 1111, 3, [1, 2])
+        "angelly@correo.com", 2000000, 110, 3, [1, 2])
     daoEnfe.guardarEnfermera(angely)
+
+    #PRUEBA CONSULTAR ENFERMERA --> FALTA
+
+    #PRUEBA BORRAR ENFERMERA --> FALTA
 
     fachada.cerrarConexion()
 
