a888028e12e4810ec34a2e033fa6c9247a6273cb
BryanTabarez <userbryan@gmail.com>
2014-12-12 18:16:10 -0500

    Actualizando..

---
+++ accesoDatos/DaoArea.py
@@ -0,0 +1,36 @@
+from logica.Area import Area
+
+
+class DaoArea():
+
+    def __init__(self, conexion):
+        self.conn = conexion
+
+    def guardarArea(self, a):
+        try:
+            cur = self.conn.cursor()
+
+            cur.execute("INSERT INTO Area(nombre, descripcion) VALUES (%s, %s)",
+                (a.get_nombre_area(), a.get_descripcion()))
+
+            cur.close()
+            self.conn.commit()
+        except:
+
+
+    def borrarArea(self, codigo):
+        cur = self.conn.cursor()
+
+        cur.execute("DELETE FROM Area WHERE codigo = %s", (codigo,))
+
+        cur.close()
+        self.conn.commit()
+
+
+
+    def consultarArea(self, codigo):
+        cur = self.conn.cursor()
+
+        cur.close()
+        self.conn.commit()
+
