43a722385c523f21a96263f7d71cd8834f6978ad
BryanTabarez <userbryan@gmail.com>
2014-12-12 19:12:49 -0500

    se sube el dao de medicamentos

--- accesoDatos/DaoMedicamento.py
+++ accesoDatos/DaoMedicamento.py
@@ -1,1 +1,15 @@
+<<<<<<< HEAD
+#from logica. import
+
+
+
+#def guardarMedicamento(self, m):
+        #try:
+            ##Abrir un cursor para realizar operaciones con la base de datos
+            #cur = self.conn.cursor()
+
+            #cur.execute("INSERT INTO Medicamento VALUES (%s, %s, %s, %s)",
+                #(m.get)
+=======
 # -*- coding: utf-8 -*-
+>>>>>>> a888028e12e4810ec34a2e033fa6c9247a6273cb
