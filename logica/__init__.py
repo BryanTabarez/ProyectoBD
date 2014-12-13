c002507d634c11cb5925e25eaad31017d0e87c40
BryanTabarez <userbryan@gmail.com>
2014-12-12 23:14:11 -0500

    Actualizando orden de paquetes y modulos

--- logica/__init__.py
+++ logica/__init__.py
@@ -1,1 +1,7 @@
-# -*- coding: utf-8 -*-
+from Area import Area
+from Cama import Cama
+from Empleado import Empleado
+from Enfermera import Enfermera
+from Medico import Medico
+from Paciente import Paciente
+from Persona import Persona
