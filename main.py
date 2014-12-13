0e964f8076955d8924a06218a1afc9f194055e8e
BryanTabarez <userbryan@gmail.com>
2014-12-13 02:03:49 -0500

    Se empieza la implementacion de excepciones

--- main.py
+++ main.py
@@ -2,6 +2,15 @@ from logica import *
 from accesoDatos import *
 
 
+def mostrarReturn(resultado):
+    """Este metodo por ahora es el que se encarga de "controlar" las
+    excepciones a nivel de la base de datos (psycopg2).
+    Lo que hace es simplemente mostrar el mensaje de la excepcion capturada"""
+    if resultado is not None:
+        print("\nExcepcion controlada :) -->")
+        print((resultado.pgerror))
+
+
 def main():
     fachada = FachadaDB()
     conexion = fachada.obtenerConexion()
@@ -10,16 +19,23 @@ def main():
     daoPac = DaoPaciente(conexion)
 #==============================================================================
 
-    ##PRUEBA INSERTAR PACIENTE
+    #PRUEBA INSERTAR PACIENTE
     paciente = Paciente(110, 'Esteban Quito', 'Calle 5', '018800777',
        '2014-01-01', 'corredor de bolsa', 900)
-    daoPac.guardarPaciente(paciente)
+    result = daoPac.guardarPaciente(paciente)
+    if isinstance(result, Exception):
+        mostrarReturn(result)
 
     ##PRUEBA CONSULTAR PACIENTE
-    #paciente = daoPac.consultarPaciente(110)
-    #print((paciente.get_identificacion()))
-    #print((paciente.get_nombre()))
-    #print((paciente.get_num_seg_social()))
+    paciente2 = daoPac.consultarPaciente(110)
+    if isinstance(paciente2, Exception):
+        mostrarReturn(paciente2)
+    if paciente2 == 0:
+        print("LA CONSULTA NO ARROJO RESULTADOS :(")
+    else:
+        print((paciente2.get_identificacion()))
+        print((paciente2.get_nombre()))
+        print((paciente2.get_num_seg_social()))
 
     ##PRUEBA MODIFICAR PACIENTE --> FALTA
 
