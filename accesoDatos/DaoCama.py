0b5c4d619d02f83bd660c8b5a469387b7132ea60
BryanTabarez <userbryan@gmail.com>
2014-12-12 20:57:36 -0500

    Se actualiza

--- accesoDatos/DaoCama.py
+++ accesoDatos/DaoCama.py
@@ -1,5 +1,5 @@
-<<<<<<< HEAD
 from logica.Cama import Cama
+
 
 class DaoCama():
 
@@ -11,8 +11,8 @@ class DaoCama():
             #Abrir un cursor para realizar operaciones con la base de datos
             cur = self.conn.cursor()
 
-            cur.execute("INSERT INTO Cama (estado, descripcion, codigo_area)
-            VALUES (%s, %s, %s), (c.get_estado(), c.get_descripcion(),
+            cur.execute("""INSERT INTO Cama (estado, descripcion, codigo_area)
+            VALUES (%s, %s, %s)""", (c.get_estado(), c.get_descripcion(),
             c.get_cod_area()))
 
             # Cerrar el cursor
@@ -20,6 +20,9 @@ class DaoCama():
 
             # Hacer que los cambios en la base de datos sean permanentes
             self.conn.commit()
+        except:
+            print("Error!")
+            print("No se pudo guardar el registro en la base de datos")
 
     def borrarCama(self, id):
         cur = self.conn.cursor()
@@ -28,12 +31,32 @@ class DaoCama():
         self.conn.commit()
 
     def consultarCama(self, id):
-        try:
+        #try:
             cur = self.conn.cursor()
-            sqlConsulta = """SELECT * FORM Cama NATURAL JOIN Area WHERE num_cama = %s"""
+            sqlConsulta = """SELECT * FORM Cama NATURAL JOIN Area
+            WHERE num_cama = %s"""
             cur.execute()(sqlConsulta, (id,))
             consulta = cur.fetchone()
             if consulta is None:
-=======
-# -*- coding: utf-8 -*-
->>>>>>> a888028e12e4810ec34a2e033fa6c9247a6273cb
+                print("La consulta no arrojo resultados")
+                cur.close()
+                return 0
+            else:
+                cama = Cama(consulta[0], consulta[1], consulta[2], consulta[3])
+                cur.close()
+                return cama
+        #except:
+            #print("Error en la consulta!")
+
+    def modificarCama(self, c):
+        cur = self.conn.cursor()
+
+        sqlUpdate = """UPDATE Cama SET estado = %s, descripcion = %s,
+        codigo_area = %s WHERE num_cama = %s"""
+
+        cur.execute(sqlUpdate, (c.get_estado(), c.get_descripcion(),
+        c.get_cod_area(), c.get_num_cama(),))
+
+        self.conn.commit()
+        cur.close()
+
