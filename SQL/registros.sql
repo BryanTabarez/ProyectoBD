128af543da079747a576c374e264a36807a23db1
BryanTabarez <userbryan@gmail.com>
2014-12-13 22:04:27 -0500

    agregando registros...

--- SQL/registros.sql
+++ SQL/registros.sql
@@ -3,8 +3,8 @@
 --=======================================================================================
 
 -- INSERTAR AREAS DEL HOSPITAL: Area (codigo, nombre, descripcion)
-INSERT INTO Area(nombre, descripcion) VALUES ('Urgencias', 'Atención integral del paciente que requiere atención médica de emergencia.');
-INSERT INTO Area(nombre, descripcion) VALUES ('Pediatria', 'Cirugías ambulatorias y Hospitalización desde Recién Nacido hasta menores de 16 años.');
+INSERT INTO Area(nombre, descripcion) VALUES ('Urgencias', 'Atencion integral del paciente que requiere atencion medica de emergencia.');
+INSERT INTO Area(nombre, descripcion) VALUES ('Pediatria', 'Cirugias ambulatorias y Hospitalizacion desde Recien Nacido hasta menores de 16 años.');
 INSERT INTO Area(nombre, descripcion) VALUES ('Ginecologia', 'Consulta de Ginecología General | Consulta de Ginecología Oncológica | Consulta de Ginecología Urológica | Consulta de Menopausia');
 INSERT INTO Area(nombre, descripcion) VALUES ('Cirujia', 'El Departamento de Cirugía es el órgano de línea final, que se encarga de prestar atención médica integral a los pacientes con enfermedades médico-quirúrgicas mediante acciones de promoción, prevención, recuperación y rehabilitación en forma individual y colectiva.')
 
@@ -24,3 +24,9 @@ INSERT INTO Empleado VALUES (110,  1, 'antonio@correo.com', 2500000, 110);
 
 INSERT INTO Habilidad (descripcion) VALUES ('cuidar viejitos');
 INSERT INTO Habilidad (descripcion) VALUES ('inyectar');
+
+-- Acetaminofeno (Rectal)
+-- se usa para aliviar el dolor y reducir la fiebre. A diferencia de la aspirina, no alivia el enrojecimiento, la rigidez o la hinchazon causados por la artritis reumatoidea. Sin embargo, puede aliviar el dolor causado por formas leves de artritis. 
+
+-- Albendazol (Oral)
+-- se usa para tratar las infecciones por lombrices causadas por ascarides, anquilostomas, oxiuros, tenias y nematodos comunes. Tambien se usa para tratar infecciones por lombrices llamadas estrongiloidiasis y la enfermedad hidatidica (tenias de perro que pueden infectar a los seres humanos). 
