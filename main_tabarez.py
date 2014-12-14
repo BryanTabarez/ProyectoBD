8ae97859cef66a0ae69a8b2e9342d64638a5e56f
BryanTabarez <userbryan@gmail.com>
2014-12-14 04:25:18 -0500

    actualizando...

--- main_tabarez.py
+++ main_tabarez.py
@@ -1,6 +1,11 @@
 from logica import *
 from accesoDatos import *
 
+import sys
+from PyQt4.QtGui import *
+from PyQt4.QtCore import *
+from PyQt4 import uic
+
 
 #==============================================================================
 def mostrarReturn(resultado):
@@ -8,7 +13,7 @@ def mostrarReturn(resultado):
     excepciones a nivel de la base de datos (psycopg2).
     Lo que hace es simplemente mostrar el mensaje de la excepcion capturada"""
     if resultado is not None:
-        print("\nExcepcion capturada -->")
+        print("\nExcepcion capturada! Falta Implementar juemadre!!")
         print((resultado.pgerror))
 #==============================================================================
 
@@ -60,6 +65,8 @@ def pruebasDaoEnfermera(conexion):
 
     # CONSULTAR ENFERMERA
 
+    # MODIFICAR ENFERMERA
+
     # BORRAR ENFERMERA
     deleteEnf = daoEnfe.borrarEnfermera(114230)
     if isinstance(deleteEnf, Exception):
@@ -102,32 +109,103 @@ def pruebasDaoArea(conexion):
 
 
 #==============================================================================
-def pruebasDaoMedicamento(conexion):
-    daoDrug = DaoMedicamento(conexion)
+class ControlDaoMedicamento():
 
-    # INSERTAR MEDICAMENTO
-    nwDrug = Medicamento("ACR01", 700, "Acetaminofeno (Rectal)",
-        """se usa para aliviar el dolor y reducir la fiebre. A diferencia de la
-        aspirina, no alivia el enrojecimiento, la rigidez o la hinchazon
-        causados por la artritis reumatoidea. Sin embargo, puede aliviar el
-        dolor causado por formas leves de artritis.""")
-    insertDrug = daoDrug.guardarMedicamento(nwDrug)
-    if isinstance(insertDrug, Exception):
-        mostrarReturn(insertDrug)
+    def __init__(self, conexion):
+        self.daoDrug = DaoMedicamento(conexion)
+
+    def insertarMedicamento(self, codigo, costo, nombre, descripcion):
+        nwDrug = Medicamento(codigo, costo, nombre, descripcion)
+        insertDrug = self.daoDrug.guardarMedicamento(nwDrug)
+        if isinstance(insertDrug, Exception):
+            mostrarReturn(insertDrug)
+        else:
+            return insertDrug
+
+    def consultarMedicamento(self, codigo):
+        drug = self.daoDrug.consultarMedicamento(codigo)
+        if isinstance(drug, Exception):
+            mostrarReturn(drug)
+        if drug is 1:
+            return 1
+        else:
+            return [drug.get_costo(), drug.get_nombre(), drug.get_descripcion()]
 #==============================================================================
 
 
 #==============================================================================
 def main():
+    app = QApplication(sys.argv)
+
+    # Abrir una conexion en la base de datos
     fachada = FachadaDB()
     conexion = fachada.obtenerConexion()
 
-    #pruebasDaoPaciente(conexion)
-    #pruebasDaoEnfermera(conexion)
-    #pruebasDaoArea(conexion)
-    pruebasDaoMedicamento(conexion)
+    mostrarVentana = VentanaMedicamento(None, conexion)
+    mostrarVentana.show()
+    r = app.exec_()
 
+    # Cerrar la conexion
     fachada.cerrarConexion()
+
+    print(("Fin " + str(r)))
+    sys.exit(r)
 #==============================================================================
 
+VmClass, InterfazAdministradorInterfazBase_class = uic.loadUiType('untitled.ui')
+
+
+class VentanaMedicamento(QMainWindow, VmClass):
+
+    def __init__(self, parent, conexion):
+        QMainWindow.__init__(self, parent)
+        self.setupUi(self)
+
+        # CENTRAR EL QMainWindow
+        pantalla = QDesktopWidget().screenGeometry()
+        interfaz = self.geometry()
+        pos_horizontal = (pantalla.width() - interfaz.width()) / 2
+        pos_vertical = (pantalla.height() - interfaz.height()) / 2
+        self.move(pos_horizontal, pos_vertical)
+
+        # SENIALES Y SLOTS
+        self.connect(self.pushButtonInsertar, SIGNAL("clicked()"),
+            self.pruebaInsertarMedicamento)
+        self.connect(self.pushButtonConsultar, SIGNAL("clicked()"),
+            self.pruebaBuscarMedicamento)
+        self.connect(self.pushButtonLimpiar, SIGNAL("clicked()"),
+            self.limpiarCampos)
+
+        # CREAR EL CONTROLADOR DEL DAO MEDICAMENTO
+        self.ctrlDaoMedicamento = ControlDaoMedicamento(conexion)
+
+    def limpiarCampos(self):
+        self.lineEditCodigo.clear()
+        self.lineEditCosto.clear()
+        self.lineEditNombre.clear()
+        self.lineEditDescripcion.clear()
+
+    def pruebaInsertarMedicamento(self):
+        codigo = str(self.lineEditCodigo.text())
+        costo = str(self.lineEditCosto.text())
+        nombre = str(self.lineEditNombre.text())
+        descripcion = str(self.lineEditDescripcion.text())
+
+        insert = self.ctrlDaoMedicamento.insertarMedicamento(codigo, costo,
+            nombre, descripcion)
+
+        self.limpiarCampos()
+
+        if insert is 0:
+            self.lineEditMensaje.setText("Se agrego el registro en la BD!")
+
+    def pruebaBuscarMedicamento(self):
+        codigo = str(self.lineEditCodigo.text())
+        result = self.ctrlDaoMedicamento.consultarMedicamento(codigo)
+        if type(result) is list:
+            self.lineEditCosto.setText(result[0])
+            self.lineEditNombre.setText(result[1])
+            self.lineEditDescripcion.setText(result[2])
+        if result is 1:
+            self.lineEditMensaje.setText("La consulta no arrojo resultados!")
 main()
