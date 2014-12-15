c50b7c7fd1d0a7771f6d4b6829190e8fb0637d76
BryanTabarez <userbryan@gmail.com>
2014-12-15 00:38:16 -0500

    actualizacion de cama

--- logica/Cama.py
+++ logica/Cama.py
@@ -1,10 +1,18 @@
 class Cama():
     """Clase Cama"""
-    def __init__(self, num_cama, estado, descripcion, cod_area):
-        self.__num_cama = num_cama
-        self.__estado = estado
-        self.__descripcion = descripcion
-        self.__cod_area = cod_area
+    """Puesto que Cama tiene num_cama como una secuencia no se debe almacenar
+    para ingresar pero si se debe almacenar para la consulta y en todo ademas
+    se debe almacenar estado, descripcion, cod_area"""
+    def __init__(self, *args):
+        if len(args) is 4:
+            self.__num_cama = args[0]
+            self.__estado = args[1]
+            self.__descripcion = args[2]
+            self.__cod_area = args[3]
+        if len(args) is 3:
+            self.__estado = args[0]
+            self.__descripcion = args[1]
+            self.__cod_area = args[2]
 
     def get_num_cama(self):
         return self.__num_cama
