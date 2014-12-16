7b8720df75d205f466166e2c19fdf00805f5a498
BryanTabarez <userbryan@gmail.com>
2014-12-16 02:03:56 -0500

    rutas paquetes, modulos, clases...

--- control/__init__.py
+++ control/__init__.py
@@ -1,6 +1,3 @@
 from ControlDaoArea import ControlDaoArea
-<<<<<<< HEAD
 from ControlDaosEmpleados import ControlDaosEmpleados
-=======
-from ControlDaoMedico import ControlDaoMedico
->>>>>>> 9567dde4587613843fd5a3f1b470bb9c8e156f9b
+#from ControlDaoMedico import ControlDaoMedico
