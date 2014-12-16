0f0ca9f61372f6ce3883f12fd002858ff7257618
aureavm <aurelio.vivas@correounivalle.edu.co>
2014-12-16 02:35:35 -0500

    Nuevos init

--- control/__init__.py
+++ control/__init__.py
@@ -1,7 +1,7 @@
 from ControlDaoArea import ControlDaoArea
-<<<<<<< HEAD
+
 from ControlDaoPaciente import ControlDaoPaciente
-=======
+
 from ControlDaosEmpleados import ControlDaosEmpleados
 #from ControlDaoMedico import ControlDaoMedico
->>>>>>> 7b8720df75d205f466166e2c19fdf00805f5a498
+
