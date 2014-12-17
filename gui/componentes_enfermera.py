fcb207962fc154d2b851fb73761426365488786f
aureavm <aurelio.vivas@correounivalle.edu.co>
2014-12-16 21:11:38 -0500

    Interfaz Enfermera Creacion de pacientes y Asignacion de camas

--- gui/componentes_enfermera.py
+++ gui/componentes_enfermera.py
@@ -2,7 +2,7 @@ from PyQt4.QtGui import *
 from PyQt4.QtCore import *
 from PyQt4 import uic
 from componentes_administrador import *
-
+import psycopg2
 
 #=======================================================================================================================
 # INTEGRANTES:
@@ -38,10 +38,11 @@ class DialogPaciente( QDialog, D_Paciente_class ):
 		self.connect( self.pushButtonInsertar, SIGNAL( "clicked()" ), self.insertarActualizarPaciente )
 		self.connect( self.pushButtonCancelar, SIGNAL( "clicked()" ), self.limpiarCampos )
 		self.connect( self.pushButtonConsultar, SIGNAL( "clicked()" ), self.consultar ) 
+		self.connect( self.pushButtonEliminar, SIGNAL( "clicked()" ), self.eliminar )
 		
 		#============================================> MODIFICACIONES 
 
-		self.lineEditIdentificacionPaciente.setReadOnly( True )
+		#self.lineEditIdentificacionPaciente.setReadOnly( True )
 		self.lineEditNumeroDeHistoria.setReadOnly( True )
 		self.dateEditFechaApertura.setReadOnly( True )
 
@@ -50,12 +51,15 @@ class DialogPaciente( QDialog, D_Paciente_class ):
 			self.setWindowTitle( "Nuevo Paciente" )
 			self.pushButtonInsertar.setText( "Insertar" )
 			self.pushButtonConsultar.hide()
+			self.pushButtonEliminar.hide()
 			
 		else:
 
 			self.setWindowTitle( "Modificar Paciente" )
 			self.pushButtonInsertar.setText( "Actualizar" )
 			self.pushButtonConsultar.show()
+			self.pushButtonEliminar.show()
+			self.pushButtonEliminar.setEnabled (False)
 
 	#=====================================================> METODOS
 	def insertarActualizarPaciente( self ):
@@ -76,16 +80,24 @@ class DialogPaciente( QDialog, D_Paciente_class ):
 			resultado = self.controladorPaciente.ingresarPaciente( identificacion, nombre, direcccion, telefono, 
 				fecha_nacimiento, actividad_economica, nss )
 
-			self.dialogInformacion.showMensaje( "Ingresar Paciente", resultado )
+			if isinstance ( resultado, Exception ):
+				self.dialogInformacion.showMensaje( "Ingresar Paciente", resultado.pgerror )
+			else:
+				self.dialogInformacion.showMensaje( "Ingresar Paciente", "El paciente fue ingresado con exito" )
+				self.limpiarCampos()
 			
 		else:
 			#EN CASO DE QUE SEA UNA ACTUALIZACION
 			resultado = self.controladorPaciente.modificarPacinete(identificacion, nombre, direcccion, telefono, 
 				fecha_nacimiento, actividad_economica, nss )
-			
-			self.dialogInformacion.showMensaje( "Ingresar Paciente", resultado )
 
-		self.limpiarCampos()
+			if isinstance ( resultado, Exception ):
+				self.dialogInformacion.showMensaje( "Actualizar Paciente", resultado.pgerror )
+			else:
+				self.dialogInformacion.showMensaje( "Actualizar Paciente", "El Paciente fue actualizado con exito" )
+				self.limpiarCampos()
+	
+		
 
 	def limpiarCampos( self ):
 
@@ -96,16 +108,23 @@ class DialogPaciente( QDialog, D_Paciente_class ):
 		self.lineEditActividadEconomica.setText( "" )
 		self.lineEditNumeroDeSeguridadSocial.setText( "" )
 
+	
 	def consultar( self ):
 		id_paciente = str( self.lineEditIdentificacion.text() )
 		datos_paciente = self.controladorPaciente.consultar( id_paciente )
 		
-		if datos_paciente == 0:
+		if isinstance (datos_paciente, Exception):
+
+			self.dialogInformacion.showMensaje( "Consultar Paciente", datos_paciente.pgerror )
+
+		elif datos_paciente == 0:
 
 			self.dialogInformacion.showMensaje( "Consultar Paciente", 
-				"El paciente no se encoentra en la base de datos" )
+				"El paciente no se encuentra en la base de datos" )
 
 		else:	
+
+			self.pushButtonEliminar.setEnabled (True)
 		
 			self.lineEditIdentificacion.setText( str( datos_paciente[0] ) )
 			self.lineEditNombre.setText( datos_paciente[1] )
@@ -121,53 +140,15 @@ class DialogPaciente( QDialog, D_Paciente_class ):
 			self.lineEditNumeroDeSeguridadSocial.setText( str(datos_paciente[6]) )
 
 
+	def eliminar( self ):
 
+		id_paciente = str(self.lineEditIdentificacion.text())
+		resultado = self.controladorPaciente.borrarPaciente( id_paciente )
 
-#===================================================> Listar Pacientes 
-
-W_ListarPacientes_class , W_LsitarPacientesBase_class = uic.loadUiType( 'gui/enfermera_uis/WidgetListarPacientes.ui' )
-
-class WidgetListarPacientes( QWidget, W_ListarPacientes_class ):
-
-	def __init__( self, controlador=None, parent=None ):
-
-		#Constructor padre
-		QWidget.__init__( self, parent )
-		#Configuracion interfaz
-		self.setupUi( self )
-
-
-
-		#======================================= VARIABLES
-		self.controladorPaciente = controlador
-
-	#==================================================> METODOS 
-
-	def actualizar( self ):
-
-
-		matriz_pacientes = self.controladorPaciente.listarPacientes()
-
-		if matriz_pacientes == 0:
-			self.dialogInformacion( "Listar Pacientes",
-				"No hay regisros de pacientes en la base de datos" )
-		else:	
-
-			for i in range(0, self.tableWidgetPacientes.rowCount()):
-				self.tableWidgetPacientes.removeRow(0)
-			
-			for row in matriz_pacientes:
-				
-				self.tableWidgetPacientes.insertRow( 0 )
-				self.tableWidgetPacientes.setItem( 0, 0, QTableWidgetItem( str( row[0] ) ) )
-				self.tableWidgetPacientes.setItem( 0, 1, QTableWidgetItem( row[1] ) )
-				self.tableWidgetPacientes.setItem( 0, 2, QTableWidgetItem( row[2] ) )
-				self.tableWidgetPacientes.setItem( 0, 3, QTableWidgetItem( row[3] ) )
-				self.tableWidgetPacientes.setItem( 0, 4, QTableWidgetItem( row[4].strftime(("%d/%m/%y")) ) )
-				self.tableWidgetPacientes.setItem( 0, 5, QTableWidgetItem( row[5] ) )
-				self.tableWidgetPacientes.setItem( 0, 6, QTableWidgetItem( str( row[6] ) ) )
-				
-
+		if isinstance (resultado, Exception):
+			self.dialogInformacion.showMensaje( "Eliminar Paciente" , resultado.pgerror )
+		else:
+		  	self.dialogInformacion.showMensaje( "Eliminar Paciente", "El paciente fue eliminado con exito" )
 
 
 #====================================================> CAMAS <==========================================================
@@ -176,7 +157,11 @@ D_Asignar_Cama_class , D_Asignar_Cama_Base_class = uic.loadUiType( 'gui/enfermer
 
 class DialogCama( QDialog, D_Asignar_Cama_class ):
 
-	def __init__( self, asignar=True, controladorPaciente=None,controladorCama=None, parent=None):
+	def __init__( self, asignar=True, 
+		controladorCama=None, 
+		controladorPaciente=None,
+		controladorCamaPaciente=None, 
+		parent=None):
 
 		#Constructor padre
 		QDialog.__init__( self, parent )
@@ -185,10 +170,14 @@ class DialogCama( QDialog, D_Asignar_Cama_class ):
 
 		#========================================> VARIABELS 
 		self.dialogInformacion  = DialogInformacion( self )
+		self.controladorCama = controladorCama
 		self.controladorPaciente = controladorPaciente
-		self.contrladorCama = controladorCama
+		self.controladorCamaPaciente = controladorCamaPaciente
 		self.asignar = asignar
-		
+		self.header1 = QStringList()
+		self.header1 << "Numero Cama" << "Descripcion" << "Codigo Area" << "Area"
+		self.header2 = QStringList()
+		self.header2 << "Numero Cama" << "Area"
 		#=========================================> SENIALES Y SLOTS
 		self.connect( self.pushButtonConsultar, SIGNAL( "clicked()" ), self.consultar )
 		self.connect( self.pushButtonAsignar, SIGNAL( "clicked()" ), self.asignarLiberarCama  )
@@ -199,53 +188,137 @@ class DialogCama( QDialog, D_Asignar_Cama_class ):
 
 			self.setWindowTitle( "Asignar Cama Paciente" )
 			self.pushButtonAsignar.setText( "Asignar" )
+			self.labelCamas.setText( "Camas disponibles por areas" )
+			self.tableWidgetCamas.insertColumn( 0 )
+			self.tableWidgetCamas.insertColumn( 0 )
+			self.tableWidgetCamas.insertColumn( 0 )
+			self.tableWidgetCamas.insertColumn( 0 )
+			self.tableWidgetCamas.setHorizontalHeaderLabels( QStringList( self.header1 ) )
 			
 		else:
 
 			self.setWindowTitle( "Liberar Cama Paciente" )
 			self.pushButtonAsignar.setText( "Liberar" )
+			self.labelCamas.setText( "Cama asignada a paciente" )
+			self.tableWidgetCamas.insertColumn( 0 )
+			self.tableWidgetCamas.insertColumn( 0 )
+			self.tableWidgetCamas.setHorizontalHeaderLabels( QStringList( self.header2 ) )
+	
 			
 
 		
 	#===========================================> METODOS
 
-	def actualizar( self ):
-
-		#AQUI SE ACTUALIZAN LOS DATOS DEL COMBO DE AREAS 
-
-		#AQUI SE LISTAN LAS CAMAS DISPONIBLES
-		#self.tableWidgetCamas.clearContents()
-		#self.tableWidgetCamas.insertRow( 0 )
-		#self.tableWidgetCamas.setItem( 0, 0, QTableWidgetItem( "Codigo" ) )
-		#self.tableWidgetCamas.setItem( 0, 1, QTableWidgetItem( "descripcion" ) )
-		pass
-
 	
-
 	def consultar( self ):
-		#AQUI SE CONSULTA LA IDENTIFICACION DEL PACIENTE AL QUIE SE LE VA ASIGNAR CAMA
-		#identificacon_consultar = str( self.lineEditIndetificacion.text() )
-		#self.lineEditNombre.setText( "Nombre paciente" )
+		id_paciente = str( self.lineEditIdentificacion.text() )
+		datos_paciente = self.controladorPaciente.consultar( id_paciente )
 
-		pass
+		if isinstance ( datos_paciente, Exception ):
+			self.dialogInformacion.showMensaje( "Consultar Paciente", datos_paciente.pgerror )
+		if datos_paciente == 1:
+			self.dialogInformacion.showMensaje( "Consultar Paciente", 
+				"El paciente no se encuentra en la base de datos" )
+			self.limpiarCampos()
+
+		else:	
+				
+			self.lineEditIdentificacion.setText( str( datos_paciente[0] ) )
+			self.lineEditNombre.setText( str(datos_paciente[1]) )
+			self.actualizarTablaCamasDisponibles(id_paciente)
+
+
 	
+	def actualizarTablaCamasDisponibles( self, id_paciente ):
+
+		if self.asignar:
+			#AQUI SE LISTAN LAS CAMAS DISPONIBLES
+			matriz_camas = self.controladorCama.listarCamas(True)
+
+			if isinstance ( matriz_camas, Exception ):
+				self.dialogInformacion.showMensaje( "Actualizar Camas Disponibles",
+					matriz_camas.pgerror )
+			else:
+
+				for i in range(0, self.tableWidgetCamas.rowCount()):
+					self.tableWidgetCamas.removeRow(0)
+				
+				print matriz_camas				
+				for row in matriz_camas:
+				
+					self.tableWidgetCamas.insertRow( 0 )
+					self.tableWidgetCamas.setItem( 0, 0, QTableWidgetItem( str(row[0]) ) )
+					self.tableWidgetCamas.setItem( 0, 1, QTableWidgetItem( str(row[1]) ) )
+					self.tableWidgetCamas.setItem( 0, 2, QTableWidgetItem( str(row[2]) ) )
+					self.tableWidgetCamas.setItem( 0, 3, QTableWidgetItem( str(row[3]) ) )
+	
+			
+		else:
+			#AQUI SE LISTA LA CAMA QUE TIENE EL PACIENTE SI LA TIENE ASIGNADA
+			matriz_camas = self.controladorCamaPaciente.consultarCamaPaciente(id_paciente)
+			
+			if isinstance ( matriz_camas, Exception ):
+				self.dialogInformacion.showMensaje( "Liberar Cama ", 
+					matriz_camas.pgerror )
+
+			elif matriz_camas == 1:
+				self.dialogInformacion.showMensaje( "Liberar Cama", 
+					"No se encontro infromacion del usuario en la base de datos")
+
+
+			else:
+
+				for i in range(0, self.tableWidgetCamas.rowCount()):
+					self.tableWidgetCamas.removeRow(0)
+				
+				for row in matriz_camas:
+					
+					self.tableWidgetCamas.insertRow( 0 )
+					self.tableWidgetCamas.setItem( 0, 0, QTableWidgetItem( str(row[0]) ) )
+					self.tableWidgetCamas.setItem( 0, 1, QTableWidgetItem( row[1] ) )
+					
+			
 
 	def asignarLiberarCama( self ):
-		print("Asignar cama")
+		
 		fila_seleccionada = self.tableWidgetCamas.currentRow()
 		if fila_seleccionada > -1:
-			identificacon = str( self.lineEditIndetificacion.text() ) 
-			area_cama = str( self.comboBoxArea.currentText() )
+			identificacon = str( self.lineEditIdentificacion.text() ) 
 			num_cama = str ( self.tableWidgetCamas.item( fila_seleccionada, 0 ).text() )
 
 			if self.asignar:
 				#AQUI SE ASIGNA LA CAMA AL PACIENTE
-				pass
+				(asg_cama, cambio_estado) = self.controladorCamaPaciente.ingresarCamaPaciente( 
+					num_cama, identificacon, "now()")
 				
+				if isinstance ( asg_cama, Exception ):
+					
+					self.dialogInformacion.showMensaje( "Asignar Cama", asg_cama.pgerror )
+				
+				elif isinstance ( cambio_estado, Exception ):
+
+					self.dialogInformacion.showMensaje( "Cambiar estado cama Ocupada", cambio_estado.pgerror )
+
+				else:
+
+					self.dialogInformacion.showMensaje( "Asignar Cama", "La cama fue asignada con exito" )
+
+					self.actualizarTablaCamasDisponibles(identificacon)
 				
 			else:
 				#AQUI SE LIBERA LA CAMA DEL PACIENTE
-				pass			
+				
+				r_liberar_cama = self.controladorCamaPaciente.liberarCama( num_cama )
+
+				if isinstance ( r_liberar_cama , Exception ):
+
+					self.dialogInformacion.showMensaje( "Liberar Cama", r_liberar_cama.pgerror )
+
+				else: 
+
+					self.dialogInformacion.showMensaje( "Liberar Cama" , "La cama fue liberada con exito")
+
+					self.actualizarTablaCamasDisponibles(identificacon)
 		
 		else:
 
@@ -261,7 +334,7 @@ class DialogCama( QDialog, D_Asignar_Cama_class ):
 
 	def limpiarCampos( self ):
 
-		self.lineEditIndetificacion.setText( "" )
+		self.lineEditIdentificacion.setText( "" )
 		self.lineEditNombre.setText( "" )
 		
 		
@@ -272,7 +345,12 @@ D_CitaInterfaz_class , D_CitaInterfazBase_class = uic.loadUiType( 'gui/enfermera
 
 class DialogCita( QDialog, D_CitaInterfaz_class ):
 
-	def __init__( self, nuevo_registro=True, controlador=None, parent=None ):
+	def __init__( self, 
+		nuevo_registro=True, 
+		controladorCita=None,
+		controladorMedico=None,
+		controladorHorarioMedico=None, 
+		parent=None ):
 
 		#Constructor padre
 		QDialog.__init__( self, parent )
@@ -282,7 +360,9 @@ class DialogCita( QDialog, D_CitaInterfaz_class ):
 		#=================================================> VARIABLES
 		self.dialogInformacion = DialogInformacion( self )
 		self.nuevo_registro = nuevo_registro
-		self.controladorCita = controlador
+		self.controladorCita = controladorCita
+		self.controladorMedico = controladorMedico
+		self.controladorHorarioMedico = controladorHorarioMedico
 
 		#=================================================> SENIALES Y SLOTS
 		self.connect( self.pushButtonConsultar, SIGNAL( "clicked()" ), self.consultar )
@@ -297,24 +377,45 @@ class DialogCita( QDialog, D_CitaInterfaz_class ):
 		
 			self.setWindowTitle( "Nueva Cita" )
 			self.pushButtonAsignar.setText( "Asignar" )
-			
+
 			self.cargarHorariosMedicos()
 
 		else: 
 
 			self.setWindowTitle( "Cancelar Cita" )
 			self.pushButtonAsignar.setText( "Cancelar Cita" )
-			
 			self.cargarCitasPaciente()
 
+		self.cargarEspecialidadesMedico()
 
 	#====================================================> METODOS
+
+	def cargarEspecialidadesMedico( self ):
+
+		especialidades = self.controladorMedico.listarEspecialidades()
+
+		if isinstance (especialidades, Exception):
+
+			self.dialogInformacion.showMensaje( "Cita", 
+				"No fue posible cargar las especialidades de los medicos de la base de datos" )
+
+		else:
+
+			for row in especialidades:
+		 		self.comboBoxHorariosDisponibles.addItem( row[0] )
+
+
 
 	def cargarHorariosMedicos( self ):
 
 		encabezado = QStringList()
-		encabezado << "Identificacion Horario" << "Identificacion Medico" << "Fecha y Hora"
+		encabezado << "Identificacion Horario" << "Medico" << "Fecha y Hora"
+		self.tableWidgetHorariosDisponibles.insertColumn( 0 )
+		self.tableWidgetHorariosDisponibles.insertColumn( 0 )
+		self.tableWidgetHorariosDisponibles.insertColumn( 0 ) 
 		self.tableWidgetHorariosDisponibles.setHorizontalHeaderLabels( encabezado )
+		#especialidad = self.
+		#horarios = self.controladorHorarioMedico.
 
 		#AQUI SE LISTAN LAS CAMAS EN LA TABLA DE CAMAS 
 		#self.tableWidgetMedicamentos.clearContents()
