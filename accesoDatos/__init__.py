de8122ea4d69efc44f981662e8a5b62f26b00001
aureavm <aurelio.vivas@correounivalle.edu.co>
2014-12-16 21:07:13 -0500

    Actualizacion de rutas

--- accesoDatos/__init__.py
+++ accesoDatos/__init__.py
@@ -3,5 +3,9 @@ from DaoCama import DaoCama
 from DaoCausa import DaoCausa
 from DaoEnfermera import DaoEnfermera
 from DaoPaciente import DaoPaciente
+from DaoMedico import DaoMedico
+from DaoHorarioMedico import DaoHorarioMedico
+from DaoCamaPaciente import DaoCamaPaciente
 from FachadaDB import FachadaDB
 from DaoMedicamento import DaoMedicamento
+from DaoCita import DaoCita
