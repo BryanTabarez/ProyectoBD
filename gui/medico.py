261c9be60b392ea3f281f6f240d805f55b21bd1d
BryanTabarez <userbryan@gmail.com>
2014-12-16 23:28:10 -0500

    se inserta el medico de la gui

---
+++ gui/medico.py
@@ -0,0 +1,21 @@
+from PyQt4.QtGui import *
+from PyQt4.QtCore import *
+from PyQt4 import uic
+
+#from componentes_enfermera import *
+
+from componentes_administrador import WidgetListarCamas
+
+from componentes_enfermera import DialogPaciente
+
+
+from componentes_enfermera import DialogCama
+from componentes_administrador import WidgetListarCamas
+
+from componentes_enfermera import DialogCita
+
+from componentes_administrador import DialogInformacion
+from componentes_enfermera import DialogAgendaMedico
+
+from accesoDatos import *
+from control import *
